085b5e11c92493027a0be745343b24a8920ecec4
leonardo08x <sylky18@gmail.com>
2025-08-12 21:46:43 -0300

    feat: pasta e modulos base criados

---
+++ modulos/modulo_de_computação.py
@@ -0,0 +1,7 @@
+def computação(chave,palavra,modo):
+    # abaixo a chamada do modulo de transições, enquanto ainda não estiver pronto crie um dicinario que relacione uma letra a outra exemplo {a}:b 
+    # produção = transições(chave, modo)
+    # voce deve pegar a palavra recevido e traduzir cada letra para seu equivalente no dicionario
+    # exemplo: casa faça um loop que passe por cada letra, identifica a chave no dicionare e salve a letra traduzida em uma variavel palavra traduzida que vai receber letra a letra da palavra nova
+    # o retorno dessa função é a palavra traduzida
+    print("reali eh day")
