a582fc1aad12b4458e2199f239c90cd800634b34
leonardo08x <sylky18@gmail.com>
2025-08-15 00:29:01 -0300

    feat: modulo de transições finalizado

---
+++ modulos/modulo_de_transições.py
@@ -0,0 +1,23 @@
+import random
+def lista_ascii_unica(chave):
+   random.seed(sum(ord(c) for c in chave)) 
+   lista_ascii = [chr(simbolo) for simbolo in range(32,127)]
+   random.shuffle(lista_ascii)
+   return lista_ascii
+def transicoes(chave, modo):
+    return  { chave:letra for (chave,letra) in zip(lista_ascii_unica(chave) , [chr(simbolo) for simbolo in range(32,127) ],)}  if modo =="descriptografar" else { chave:letra for (chave,letra) in zip([chr(simbolo) for simbolo in range(32,127) ], lista_ascii_unica(chave))} 
+   
+
+
+#debug, checa se os valores se repetem, e se os modos geram dicionarios invertidos, REMOVER DA VERSÃO FINAL
+print(transicoes("ldfwiajfwai", "descriptografar"))
+print(transicoes("ldfwiajfwai", "criptografar"))
+valores = transicoes("ca", "gay").values()
+
+for i in valores:
+    count = 0
+    for j in valores:
+        if i == j:
+            count += 1
+            if count == 2:
+             print(i+"="+j)
